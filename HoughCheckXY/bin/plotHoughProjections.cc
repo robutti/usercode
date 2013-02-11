11fb459c5c3b52b0b00209ab1800a57cf42e623a
robutti <>
2013-02-11 14:27:15 +0000

    All fitted track parameters superimposed on vote histograms.

--- HoughCheckXY/bin/plotHoughProjections.cc
+++ HoughCheckXY/bin/plotHoughProjections.cc
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
 #include <TFile.h>
 #include <TString.h>
 #include <TH3S.h>
+#include <TTree.h>
+#include <TBranch.h>
 #include <TH2S.h>
 #include <TROOT.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <TEllipse.h>
 
 using namespace std;
 
@@ -21,7 +25,7 @@ int main(int argc, char* argv[])
   }
   char* inFileName = argv[1];
   
-  // Open input file and get 3D histogram
+  // Open input file and get objects
   TFile* inFile = new TFile(inFileName);
   if (inFile == 0) {
     cout << "Error opening input file " << inFileName << ". Abort." << endl;
@@ -32,7 +36,34 @@ int main(int argc, char* argv[])
     cout << "Error getting votes histogram. Abort." << endl;
     return 1;
   }
-  
+  TTree* tTrackPar = (TTree*)(inFile->Get("trackTree"));
+  if (tTrackPar == 0) {
+    cout << "Error getting track parameter tree. Abort." << endl;
+    return 1;
+  }
+
+  // Build all-events vectors from tree (per-event) entries
+  vector<double> vDoca, vKappa, vPhi;
+  vector<double>* vDocaEv = 0;
+  vector<double>* vKappaEv = 0;
+  vector<double>* vPhiEv = 0;
+  TBranch* bDocaEv = 0;
+  TBranch* bKappaEv = 0;
+  TBranch* bPhiEv = 0;
+  tTrackPar->SetBranchAddress("doca", &vDocaEv, &bDocaEv);
+  tTrackPar->SetBranchAddress("kappa", &vKappaEv, &bKappaEv);
+  tTrackPar->SetBranchAddress("phi", &vPhiEv, &bPhiEv);
+  // Loop on tree
+  for (int iEv = 0; iEv < tTrackPar->GetEntries(); iEv++) {
+    long tEntry = tTrackPar->LoadTree(iEv);
+    tTrackPar->GetEntry(tEntry);
+    for (int iTk = 0; iTk < vDocaEv->size(); iTk++) {
+      vDoca.push_back(vDocaEv->at(iTk));
+      vKappa.push_back(vKappaEv->at(iTk));
+      vPhi.push_back(vPhiEv->at(iTk));
+    }
+  }
+
   // Set up graphics
   TString psFileName = "houghVoteProjections.ps";
   TString openPsStr = psFileName + "[";
@@ -41,10 +72,14 @@ int main(int argc, char* argv[])
   gStyle->SetPalette(1);
   gStyle->SetOptFit(0001);
   gStyle->SetOptStat(0);
+  double maxVote = hHoughVotes->GetMaximum();
   TCanvas cHoughProj("cHoughProj", "", 800, 1200);
   TH2S* hProj[6];
   TString hName, hTitle;
-  
+  float rDoca =0.05*(hHoughVotes->GetXaxis()->GetXmax() - hHoughVotes->GetXaxis()->GetXmin());
+  float rKappa =0.05*(hHoughVotes->GetYaxis()->GetXmax() - hHoughVotes->GetYaxis()->GetXmin());
+  float rPhi =0.05*(hHoughVotes->GetZaxis()->GetXmax() - hHoughVotes->GetZaxis()->GetXmin());
+  cout << "rDoca = " << rDoca << "; rKappa = " << rKappa << "; rPhi = " << rPhi << endl;
   cHoughProj.Print(openPsStr);
 
   // phi slices
@@ -60,7 +95,22 @@ int main(int argc, char* argv[])
     hHoughVotes->GetZaxis()->SetRange(iBin, iBin);
     hProj[iPad - 1] = (TH2S*)(hHoughVotes->Project3D("yx"));
     hProj[iPad - 1]->SetNameTitle(hName, hTitle);
+    hProj[iPad - 1]->SetMinimum(0.);
+    hProj[iPad - 1]->SetMaximum(maxVote);
     hProj[iPad - 1]->Draw("COLZ");
+    // Draw circles around fitted track parameters
+    for (int iTkPar = 0; iTkPar < vPhi.size(); iTkPar++) {
+      if (vPhi.at(iTkPar) > hHoughVotes->GetZaxis()->GetBinLowEdge(iBin) &&
+	  vPhi.at(iTkPar) <= hHoughVotes->GetZaxis()->GetBinUpEdge(iBin)) {
+	cout << "(doca, kappa, phi) = (" << vDoca.at(iTkPar) << ", " << vKappa.at(iTkPar) << ", " << vPhi.at(iTkPar) << ")" << endl;
+	TEllipse* circle = new TEllipse(vDoca.at(iTkPar), vKappa.at(iTkPar), rDoca, rKappa);
+	circle->SetLineWidth(2);
+	circle->SetLineColor(kYellow);
+	circle->SetFillStyle(0);
+	circle->Draw();
+      }
+    }
+    // Print page if complete      
     if (iPad == 6) {
       cHoughProj.Update();
       cHoughProj.Print(psFileName);
@@ -87,7 +137,22 @@ int main(int argc, char* argv[])
     hHoughVotes->GetYaxis()->SetRange(iBin, iBin);
     hProj[iPad - 1] = (TH2S*)(hHoughVotes->Project3D("zx"));
     hProj[iPad - 1]->SetNameTitle(hName, hTitle);
+    hProj[iPad - 1]->SetMinimum(0.);
+    hProj[iPad - 1]->SetMaximum(maxVote);
     hProj[iPad - 1]->Draw("COLZ");
+    // Draw circles around fitted track parameters
+    for (int iTkPar = 0; iTkPar < vKappa.size(); iTkPar++) {
+      if (vKappa.at(iTkPar) > hHoughVotes->GetYaxis()->GetBinLowEdge(iBin) &&
+	  vKappa.at(iTkPar) <= hHoughVotes->GetYaxis()->GetBinUpEdge(iBin)) {
+	cout << "(doca, kappa, phi) = (" << vDoca.at(iTkPar) << ", " << vKappa.at(iTkPar) << ", " << vPhi.at(iTkPar) << ")" << endl;
+	TEllipse* circle = new TEllipse(vDoca.at(iTkPar), vPhi.at(iTkPar), rDoca, rPhi);
+	circle->SetLineWidth(2);
+	circle->SetLineColor(kYellow);
+	circle->SetFillStyle(0);
+	circle->Draw();
+      }
+    }
+    // Print page if complete      
     if (iPad == 6) {
       cHoughProj.Update();
       cHoughProj.Print(psFileName);
@@ -114,7 +179,22 @@ int main(int argc, char* argv[])
     hHoughVotes->GetXaxis()->SetRange(iBin, iBin);
     hProj[iPad - 1] = (TH2S*)(hHoughVotes->Project3D("zy"));
     hProj[iPad - 1]->SetNameTitle(hName, hTitle);
+    hProj[iPad - 1]->SetMinimum(0.);
+    hProj[iPad - 1]->SetMaximum(maxVote);
     hProj[iPad - 1]->Draw("COLZ");
+    // Draw circles around fitted track parameters
+    for (int iTkPar = 0; iTkPar < vDoca.size(); iTkPar++) {
+      if (vDoca.at(iTkPar) > hHoughVotes->GetXaxis()->GetBinLowEdge(iBin) &&
+	  vDoca.at(iTkPar) <= hHoughVotes->GetXaxis()->GetBinUpEdge(iBin)) {
+	cout << "(doca, kappa, phi) = (" << vDoca.at(iTkPar) << ", " << vKappa.at(iTkPar) << ", " << vPhi.at(iTkPar) << ")" << endl;
+	TEllipse* circle = new TEllipse(vKappa.at(iTkPar), vPhi.at(iTkPar), rKappa, rPhi);
+	circle->SetLineWidth(2);
+	circle->SetLineColor(kYellow);
+	circle->SetFillStyle(0);
+	circle->Draw();
+      }
+    }
+    // Print page if complete      
     if (iPad == 6) {
       cHoughProj.Update();
       cHoughProj.Print(psFileName);
