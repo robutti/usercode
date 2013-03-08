5bab0c18ecb3c32e0bbb5c0aac84bc7928e18769
robutti <>
2013-03-08 13:05:52 +0000

    Needed to make ROOT dictionary

---
+++ HoughTransChecks/src/LinkDef.h
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+#include "../interface/TH5.h"
+#else
+#include "../interface/TH5.h"
+#endif
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ class TH5;
+#pragma link C++ class TH5C;
+#endif
